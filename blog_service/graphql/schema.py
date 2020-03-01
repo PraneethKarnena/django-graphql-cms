44f763eaf895119d8077f0caec77acc465f5743e
Praneeth Karnena <praneeth.codes@gmail.com>
2020-03-01 20:50:54 +0530

    Add GraphQL Query for Comments

--- blog_service/graphql/schema.py
+++ blog_service/graphql/schema.py
@@ -6,8 +6,11 @@ from blog_service import models
 class Query(graphene.AbstractType):
 
     posts = graphene.List(types.PostType)
+    comments = graphene.List(types.CommentType)
 
 
-    def resolve_posts(self, **kwargs):
+    def resolve_posts(self, info, **kwargs):
         return models.PostModel.objects.all()
 
+    def resolve_comments(self, info, **kwargs):
+        return models.CommentModel.objects.all()
