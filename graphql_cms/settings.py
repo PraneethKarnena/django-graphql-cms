db90d06f1360e2e5e608db5f75bc77b6da7e6ebb
Praneeth Karnena <praneeth.codes@gmail.com>
2020-03-01 20:20:06 +0530

    Register graphene-django

--- graphql_cms/settings.py
+++ graphql_cms/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphene_django', # GraphQL: graphene-django
 
     'blog_service.apps.BlogServiceConfig', # Blogging app
 ]
