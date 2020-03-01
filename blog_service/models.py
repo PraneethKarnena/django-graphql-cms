908350adc5c34957e5b9b71aedf7f0d555e7de12
Praneeth Karnena <praneeth.codes@gmail.com>
2020-03-01 20:11:36 +0530

    Create Post model

--- blog_service/models.py
+++ blog_service/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class PostModel(models.Model):
+
+    title = models.CharField(max_length=255, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    publish_date = models.DateTimeField(auto_now_add=True)
+
+    author = models.TextField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
