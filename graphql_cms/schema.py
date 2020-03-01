7f65f9a484b8f1473da4768b250ea78997f0aee7
Praneeth Karnena <praneeth.codes@gmail.com>
2020-03-01 22:14:03 +0530

    Register GraphQL mutations

--- graphql_cms/schema.py
+++ graphql_cms/schema.py
@@ -1,9 +1,13 @@
 import graphene
 from blog_service.graphql.schema import Query as AppQuery
+from blog_service.graphql.schema import Mutation as AppMutation
 
 
 class Query(AppQuery, graphene.ObjectType):
     pass
 
 
-schema = graphene.Schema(query=Query)
+class Mutation(AppMutation, graphene.ObjectType):
+    pass
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
