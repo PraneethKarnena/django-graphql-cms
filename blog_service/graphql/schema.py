7198a60b1f96b1b80581f01469b2a19ed08d3f4c
Praneeth Karnena <praneeth.codes@gmail.com>
2020-03-01 21:34:45 +0530

    Implement GraphQL Query for Post

--- blog_service/graphql/schema.py
+++ blog_service/graphql/schema.py
@@ -6,11 +6,21 @@ from blog_service import models
 class Query(graphene.AbstractType):
 
     posts = graphene.List(types.PostType)
+    post = graphene.Field(types.PostType, id=graphene.Int())
+
     comments = graphene.List(types.CommentType)
 
 
     def resolve_posts(self, info, **kwargs):
         return models.PostModel.objects.all()
+
+    def resolve_post(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return models.PostModel.objects.get(pk=id)
+
+        return
 
     def resolve_comments(self, info, **kwargs):
         return models.CommentModel.objects.all()
